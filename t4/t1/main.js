2862d6689b0667806b1fb30fc53ceb3e96d325b1
osvaldo345 <42743246+osvaldo345@users.noreply.github.com>
2019-04-01 22:26:13 -0600

    Add files via upload

---
+++ t4/t1/main.js
@@ -0,0 +1,21 @@
+document.querySelector("#btnAdd").addEventListener("click", ()=>{
+    let uCuenta = document.querySelector("#cuenta").value;
+    let uNombre = document.querySelector("#nombre").value;
+
+    
+   let user = {
+       cuenta : uCuenta,
+       nombre : uNombre
+   };
+
+   let todos = [];
+   let local = JSON.parse(localStorage.getItem("credenciales-2"));
+
+   if(local != null) {
+       todos = local;
+   }
+
+   console.log(todos);
+   todos.push(user);
+   localStorage.setItem("credenciales-2", JSON.stringify(todos));
+})
